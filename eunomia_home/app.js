9a14aaf68b02cb508c63eca73c3068dceb889c44
navamivk <navamioct25@gmail.com>
2021-10-30 22:23:58 +0400

    Website frontend

---
+++ eunomia_home/app.js
@@ -0,0 +1,8 @@
+const menu = document.querySelector('#mobile-menu');
+const menuLinks = document.querySelector('.navbar__menu');
+const navLogo = document.querySelector('#navbar__logo');
+
+menu.addEventListener('click', function() {
+  menu.classList.toggle('is-active');
+  menuLinks.classList.toggle('active');
+});
